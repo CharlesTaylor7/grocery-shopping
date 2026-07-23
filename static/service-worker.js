83aa4dbf332a6f80b43a9fe6199a73b14c0b1c85
Charles Taylor <charlestaylor95@gmail.com>
2026-07-23 11:03:06 -0400

    Service worker handles offline requests

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,10 @@
-const APP_VERSION = "1";
+const ASSET_CACHE = "asset_v1";
+const DATA_CACHE = "data_v1";
 const ASSETS = ["/manifest.json", "/pwa-icon.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(APP_VERSION).then((cache) => {
+    caches.open(ASSET_CACHE).then((cache) => {
       return cache.addAll(ASSETS);
     }),
   );
@@ -15,7 +16,7 @@ self.addEventListener("activate", (event) => {
     caches.keys().then((keys) =>
       Promise.all(
         keys.map((key) => {
-          if (key !== APP_VERSION) {
+          if (key !== ASSET_CACHE && key !== DATA_CACHE) {
             return caches.delete(key);
           }
         }),
@@ -26,10 +27,33 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
+async function fetchWithCache(request) {
+  // only intercept GET requests
+  // only intercept http requests
+  if (
+    request.method !== "GET" || request.url.startsWith("chrome-extension://")
+  ) {
+    return await fetch(request);
+  }
+
+  // all offline requests use the cache
+  if (!navigator.onLine) {
+    return await caches.match(request);
+  }
+
+  const assets = await caches.open(ASSET_CACHE);
+  const asset = await assets.match(request);
+  if (asset) return asset;
+
+  // all online requests are put into the cache first before responding
+  const dataCache = await caches.open(DATA_CACHE);
+  // fetches and adds to cache any 20x status code
+  await dataCache.add(request);
+  const result = await dataCache.match(request);
+
+  return result;
+}
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
-  );
+  event.respondWith(fetchWithCache(event.request));
 });
