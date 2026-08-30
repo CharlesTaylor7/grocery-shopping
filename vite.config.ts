c38d0d125bea98377a8f7c4e6959d5818b67a389
Charles Taylor <charlestaylor95@gmail.com>
2026-08-30 16:32:13 -0400

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    watch: {
+      // Ignore the .nvim directory entirely
+      ignored: ['.nvim/**'],
+    },
   },
   build: {
     outDir: "docs",
